b2a249def3b1aeb1217fb6b5e5893bc1a419ac31
Sean Barbeau <sjbarbeau@gmail.com>
2014-09-03 12:46:46 -0400

    Add option to run geocoder with or without bounding box
    * Also increase max results to 30

--- app/src/main/java/com/barbeaudev/geocoderdemo/GeocoderDemo.java
+++ app/src/main/java/com/barbeaudev/geocoderdemo/GeocoderDemo.java
@@ -22,10 +22,11 @@ import java.util.List;
 
 public class GeocoderDemo extends Activity {
 
-    public static final int MAX_RESULTS = 10;  // Max number of results returned
+    public static final int MAX_RESULTS = 30;  // Max number of results returned
     private static final String TAG = "GeocoderDemo";
 
     Context mContext;
+    Button mRunGeocoderWithBoundingBox;
     Button mRunGeocoder;
     TextView mResults;
     EditText mInput;
@@ -37,49 +38,73 @@ public class GeocoderDemo extends Activity {
 
         mContext = this;
 
+        mRunGeocoderWithBoundingBox = (Button) findViewById(R.id.btnRunGeocoderBoundingBox);
         mRunGeocoder = (Button) findViewById(R.id.btnRunGeocoder);
         mResults = (TextView) findViewById(R.id.txtResults);
         mInput = (EditText) findViewById(R.id.editTxtInput);
 
-        mRunGeocoder.setOnClickListener(new View.OnClickListener() {
+        // Move cursor to end of EditText
+        mInput.setSelection(mInput.getText().length());
+
+        mRunGeocoderWithBoundingBox.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Geocoder gc = new Geocoder(mContext);
-
-                Log.d(TAG, "Gecoder.isPresent = " + gc.isPresent());
-
-                try {
-                    // Clear previous results
-                    mResults.setText("");
-
-                    List<CustomAddress> addresses = new ArrayList<CustomAddress>();
-                    List<Address> androidTypeAddresses;
-                    StringBuilder sb = new StringBuilder();
-
-                    androidTypeAddresses = gc.getFromLocationName(mInput.getText().toString(),
-                            MAX_RESULTS,
-                            27.6236434,  // Tampa Bay Area, FL
-                            -82.8511308,
-                            28.3251809,
-                            -82.0559399);
-
-                    // Copy addresses to custom address object, to help with formatting output
-                    for (Address androidAddress : androidTypeAddresses) {
-                        addresses.add(new CustomAddress(androidAddress));
-                    }
-
-                    // Add results to StringBuilder to be output to UI
-                    for (CustomAddress customAddress : addresses) {
-                        sb.append(customAddress.toString());
-                        sb.append(SystemUtils.LINE_SEPARATOR);
-                    }
-
-                    // Show results in UI
-                    mResults.setText(sb.toString());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                runGeocoder(true);
             }
         });
+
+        mRunGeocoder.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                runGeocoder(false);
+            }
+        });
+    }
+
+    /**
+     * Runs the geocoder
+     * @param withBoundingBox true if a bounding box should be used, false if it should not
+     */
+    private void runGeocoder(boolean withBoundingBox) {
+        Geocoder gc = new Geocoder(mContext);
+
+        Log.d(TAG, "Gecoder.isPresent = " + gc.isPresent());
+
+        try {
+            // Clear previous results
+            mResults.setText("");
+
+            List<CustomAddress> addresses = new ArrayList<CustomAddress>();
+            List<Address> androidTypeAddresses;
+            StringBuilder sb = new StringBuilder();
+
+            if (withBoundingBox) {
+                androidTypeAddresses = gc.getFromLocationName(mInput.getText().toString(),
+                        MAX_RESULTS,
+                        27.6236434,  // Tampa Bay Area, FL
+                        -82.8511308,
+                        28.3251809,
+                        -82.0559399);
+            } else {
+                androidTypeAddresses = gc.getFromLocationName(mInput.getText().toString(),
+                        MAX_RESULTS);
+            }
+
+            // Copy addresses to custom address object, to help with formatting output
+            for (Address androidAddress : androidTypeAddresses) {
+                addresses.add(new CustomAddress(androidAddress));
+            }
+
+            // Add results to StringBuilder to be output to UI
+            for (CustomAddress customAddress : addresses) {
+                sb.append('\u2022' + " ");
+                sb.append(customAddress.toString());
+                sb.append(SystemUtils.LINE_SEPARATOR);
+            }
+
+            // Show results in UI
+            mResults.setText(sb.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
